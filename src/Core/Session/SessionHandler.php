49c2f531a919960ba0f63de690923dba5ffbaab3
Patrick Strakeljahn <patrick.strakeljahn@gmail.com>
2022-02-21 22:24:10 +0100

    Prettier

--- src/Core/Session/SessionHandler.php
+++ src/Core/Session/SessionHandler.php
@@ -7,17 +7,17 @@ use PS\Source\Core\RequestHandler\Request;
 class SessionHandler extends Request
 {
     public static function loggedIn(): bool
-    {   
+    {
         $token = self::getBearerToken();
-        if(is_null($token)) {
+        if (is_null($token)) {
             return false;
         }
         $payload = TokenHelper::decodeToken($token);
-        if(is_null($payload)) {
+        if (is_null($payload)) {
             return false;
         }
         $userID = $payload['userID'];
-        if(is_numeric($userID)) {
+        if (is_numeric($userID)) {
             return true;
         }
         return false;
@@ -28,13 +28,11 @@ class SessionHandler extends Request
         $headers = null;
         if (isset($_SERVER['Authorization'])) {
             $headers = trim($_SERVER["Authorization"]);
-        } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+        } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
             $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
         } elseif (function_exists('apache_request_headers')) {
             $requestHeaders = apache_request_headers();
-            // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
             $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
-            //print_r($requestHeaders);
             if (isset($requestHeaders['Authorization'])) {
                 $headers = trim($requestHeaders['Authorization']);
             }
