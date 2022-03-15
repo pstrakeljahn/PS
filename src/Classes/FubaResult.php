5d8f2edde00bb7c449212bec06dae5591161c1cc
Patrick Strakeljahn <patrick.strakeljahn@gmail.com>
2022-03-15 22:05:58 +0100

    logic implemented

--- src/Classes/FubaResult.php
+++ src/Classes/FubaResult.php
@@ -3,6 +3,7 @@
 namespace PS\Source\Classes;
 
 use PS\Source\BasicClasses\FubaResultBasic;
+use PS\Source\Settings\Config;
 
 /*
 *   Logic can be implemented here that is not overwritten
@@ -10,5 +11,22 @@ use PS\Source\BasicClasses\FubaResultBasic;
 
 class FubaResult extends FubaResultBasic
 {
-    
+    public function getLatestResult(int $teamID = null)
+    {
+        $output = [];
+        if (is_null($teamID)) {
+            foreach (Config::ARR_TEAMS as $tID) {
+                $selfInstance = new self;
+                $result = $selfInstance->add('teamNumber', $tID)->add('goalsAway', null, 'isNotNull')->orderBy('kickoff', 'DESC')->limit(1)->select();
+                if (count($result)) {
+                    $output[] = $result[0];
+                }
+            }
+        }
+        if ($teamID) {
+            $result = $this->add('teamNumber', $teamID)->add('goalsAway', null, 'isNotNull')->orderBy('kickoff', 'DESC')->limit(1)->select()[0];
+            $output[] = count($result) ? $result[0] : [];
+        }
+        return $output;
+    }
 }
--- src/Classes/VolleyballResult.php
+++ src/Classes/VolleyballResult.php
@@ -3,6 +3,7 @@
 namespace PS\Source\Classes;
 
 use PS\Source\BasicClasses\VolleyballResultBasic;
+use PS\Source\Settings\Config;
 
 /*
 *   Logic can be implemented here that is not overwritten
@@ -10,5 +11,25 @@ use PS\Source\BasicClasses\VolleyballResultBasic;
 
 class VolleyballResult extends VolleyballResultBasic
 {
-    
+    public function getLatestResult(array $teamIDs = [])
+    {
+        $output = [];
+        if (!count($teamIDs)) {
+            foreach (array_values(Config::ARR_SEASONKEYS) as $tID) {
+                $selfInstance = new self;
+                $result = $selfInstance->add('teamID', $tID)->orderBy('kickoff', 'DESC')->limit(1)->select();
+                if (count($result)) {
+                    $output[] = $result[0];
+                }
+            }
+        }
+        if (count($teamIDs)) {
+            foreach ($teamIDs as $teamID) {
+                $selfInstance = new self;
+                $result = $selfInstance->add('teamID', $teamID)->orderBy('kickoff', 'DESC')->limit(1)->select();
+                $output[] = count($result) ? $result[0] : [];
+            }
+        }
+        return $output;
+    }
 }
